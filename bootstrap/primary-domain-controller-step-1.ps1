137f41025f2343254295603cfde79f2cc61cdf37
Rick Dehlinger <rick.dehlinger@citrix.com>
2022-05-18 13:59:57 -0700

    Revert "updated primary domain controller bootstrap"
    This reverts commit 23bfc337587832061972ac4b530f5f7d26baf67e.

--- bootstrap/primary-domain-controller-step-1.ps1
+++ bootstrap/primary-domain-controller-step-1.ps1
@@ -20,7 +20,7 @@ Function New-RandomString {
 		[char[]] $AllowedChars = $Null
 	)
 	If ($AllowedChars -eq $Null) {
-		$AllowedChars = '^$*.[]{}()?-"!@#%/\,:;|_~`abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'.toCharArray()
+		(,(33,126)) | % { For ($a=$_[0]; $a -le $_[1]; $a++) { $AllowedChars += ,[char][byte]$a } }
 	}
 	For ($i=1; $i -le $Length; $i++) {
 		$Temp += ( $AllowedChars | Get-Random )
@@ -29,7 +29,7 @@ Function New-RandomString {
 }
 Function New-RandomPassword() {
 	Param(
-		[int] $Length = 16,
+		[int] $Length = 24,
 		[char[]] $AllowedChars = $Null
 	)
 	Return New-RandomString -Length $Length -AllowedChars $AllowedChars | ConvertTo-SecureString -AsPlainText -Force
@@ -53,8 +53,47 @@ Function Get-GoogleMetadata() {
         }
 }
 
+Function Get-Setting {
+        Param (
+        [Parameter(Mandatory=$True)][String][ValidateNotNullOrEmpty()]
+        $Path   
+        )       
+        
+        $GcsPrefix = Get-GoogleMetadata -Path "instance/attributes/gcs-prefix"
+        If ($GcsPrefix.EndsWith("/")) { 
+                $GcsPrefix = $GcsPrefix -Replace ".$"
+        }
+        $Value = gsutil -q cat "$GcsPrefix/settings/$Path"
+        Return $Value
+
+}
+
+Function Set-Setting {
+        Param (
+        [Parameter(Mandatory=$True)][String][ValidateNotNullOrEmpty()]
+        $Path,
+        [Parameter(Mandatory=$True)][String][ValidateNotNullOrEmpty()]
+        $Value
+        )
+
+        $GcsPrefix = Get-GoogleMetadata -Path "instance/attributes/gcs-prefix"
+        If ($GcsPrefix.EndsWith("/")) {
+                $GcsPrefix = $GcsPrefix -Replace ".$"
+        }
+
+        $TempFile = New-TemporaryFile
+        $Value | Out-File -NoNewLine $TempFile.FullName
+        gsutil -q cp $TempFile.FullName "$GcsPrefix/settings/$Path"
+        Remove-Item $TempFile.FullName
+
+}
 
 Write-Host "Bootstrap script started..."
+
+
+# turn off gcloud version checks
+gcloud config set component_manager/disable_update_check true
+
 
 
 #Write-Host "Installing AD features in background..."
@@ -63,57 +102,10 @@ Write-Host "Installing AD features..."
 Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools
 
 
-#Write-Host "Removing external address in background..."
-#Start-Job -ScriptBlock {
-#	# windows should have activated before script is invoked, so now remove external address
-#	$name = Invoke-RestMethod -Headers @{"Metadata-Flavor" = "Google"} -Uri http://169.254.169.254/computeMetadata/v1/instance/name
-#	$zone = Invoke-RestMethod -Headers @{"Metadata-Flavor" = "Google"} -Uri http://169.254.169.254/computeMetadata/v1/instance/zone
-#	gcloud compute instances delete-access-config $name --zone $zone
-#}
-
-If ("true" -like (Get-GoogleMetadata "instance/attributes/remove-address")) {
-        Write-Host "Removing external address..."
-        $name = Get-GoogleMetadata "instance/name"
-        $zone = Get-GoogleMetadata "instance/zone"
-        gcloud compute instances delete-access-config $name --zone $zone
-}
-
-
-Write-Host "Configuring network..."
-# reconfigure dhcp address as static to avoid warnings during dcpromo
-$IpAddr = Get-NetIPAddress -InterfaceAlias Ethernet -AddressFamily IPv4
-$IpConf = Get-NetIPConfiguration -InterfaceAlias Ethernet
-Set-NetIPInterface `
-	-InterfaceAlias Ethernet `
-	-Dhcp Disabled
-New-NetIPAddress `
-	-InterfaceAlias Ethernet `
-	-IPAddress $IpAddr.IPAddress `
-	-AddressFamily IPv4 `
-	-PrefixLength $IpAddr.PrefixLength `
-	-DefaultGateway $IpConf.IPv4DefaultGateway.NextHop
-
-# set dns to google cloud default, will be set to loopback once dns feature is installed
-Set-DnsClientServerAddress -InterfaceAlias Ethernet -ServerAddresses $IpConf.IPv4DefaultGateway.NextHop
-
-# above can cause network blip, so wait until metadata server is responsive
-$HaveMetadata = $False
-While( ! $HaveMetadata ) { Try {
-	Invoke-RestMethod -Headers @{"Metadata-Flavor" = "Google"} -Uri http://169.254.169.254/ 1>$Null 2>&1
-	$HaveMetadata = $True
-} Catch {
-	Write-Host "Waiting on metadata..."
-	Start-Sleep 5
-} }
-Write-Host "Contacted metadata server. Proceeding..."
-
-
 Write-Host "Fetching metadata parameters..."
 $Domain = Invoke-RestMethod -Headers @{"Metadata-Flavor" = "Google"} -Uri http://169.254.169.254/computeMetadata/v1/instance/attributes/domain-name
 $NetBiosName = Invoke-RestMethod -Headers @{"Metadata-Flavor" = "Google"} -Uri http://169.254.169.254/computeMetadata/v1/instance/attributes/netbios-name
-$KmsKey = Invoke-RestMethod -Headers @{"Metadata-Flavor" = "Google"} -Uri http://169.254.169.254/computeMetadata/v1/instance/attributes/kms-key
 $GcsPrefix = Invoke-RestMethod -Headers @{"Metadata-Flavor" = "Google"} -Uri http://169.254.169.254/computeMetadata/v1/instance/attributes/gcs-prefix
-#$RuntimeConfig = Invoke-RestMethod -Headers @{"Metadata-Flavor" = "Google"} -Uri http://169.254.169.254/computeMetadata/v1/instance/attributes/runtime-config
 
 
 Write-Host "Configuring admin credentials..."
@@ -123,17 +115,22 @@ $LocalAdminPassword = New-RandomPassword
 Set-LocalUser Administrator -Password $LocalAdminPassword
 Enable-LocalUser Administrator
 
-Write-Host "Saving encrypted credentials in GCS..."
+Write-Host "Saving credentials in GCS..."
 If ($GcsPrefix.EndsWith("/")) {
   $GcsPrefix = $GcsPrefix -Replace ".$"
 }
 $TempFile = New-TemporaryFile
 
-Unwrap-SecureString $LocalAdminPassword | gcloud kms encrypt --key $KmsKey --plaintext-file - --ciphertext-file $TempFile.FullName
-gsutil cp $TempFile.FullName "$GcsPrefix/output/domain-admin-password.bin"
+$admin_pwd = Unwrap-SecureString $LocalAdminPassword
+$admin_pwd | Out-File -NoNewLine -Encoding "ASCII" $TempFile.FullName
+gsutil -q cp $TempFile.FullName "$GcsPrefix/output/domain-admin-password"
 
-Unwrap-SecureString $SafeModeAdminPassword | gcloud kms encrypt --key $KmsKey --plaintext-file - --ciphertext-file $TempFile.FullName
-gsutil cp $TempFile.FullName "$GcsPrefix/output/dsrm-admin-password.bin"
+$dsrm_pwd = Unwrap-SecureString $SafeModeAdminPassword
+$dsrm_pwd | Out-File -NoNewLine -Encoding "ASCII" $TempFile.FullName
+gsutil -q cp $TempFile.FullName "$GcsPrefix/output/dsrm-admin-password"
+
+@{domain="$Domain"; netbiosname="$NetbiosName"; admin_user="$NetbiosName\Administrator"; admin_password="$admin_pwd"; dsrm_password="$dsrm_pwd"} | ConvertTo-Json | Out-File -NoNewLine -Encoding "ASCII" $TempFile.FullName
+gsutil -q cp $TempFile.FullName "$GcsPrefix/output/domain-admin.json"
 
 Remove-Item $TempFile.FullName -Force
 
@@ -160,9 +157,10 @@ Install-ADDSForest @Params
 
 
 Write-Host "Configuring startup metadata..."
-$name = Invoke-RestMethod -Headers @{"Metadata-Flavor" = "Google"} -Uri http://169.254.169.254/computeMetadata/v1/instance/name
-$zone = Invoke-RestMethod -Headers @{"Metadata-Flavor" = "Google"} -Uri http://169.254.169.254/computeMetadata/v1/instance/zone
-gcloud compute instances add-metadata "$name" --zone $zone --metadata windows-startup-script-url=$GcsPrefix/bootstrap/primary-domain-controller-step-2.ps1
+$name = Get-GoogleMetadata "instance/name"
+$zone = Get-GoogleMetadata "instance/zone"
+$BootstrapFrom = Get-GoogleMetadata "instance/attributes/bootstrap-from"
+gcloud compute instances add-metadata "$name" --zone $zone --metadata windows-startup-script-url=$BootstrapFrom/primary-domain-controller-step-2.ps1
 
 
 Write-Host "Restarting computer..."
